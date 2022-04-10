aa28de6d5ec66bd72ce3aa505d30514698406264
Aleksandr1tolmachev <sanyo.ghost@mail.ru>
2022-04-10 17:33:52 +0800

    add

---
+++ Structure/index.php
@@ -0,0 +1,28 @@
+<html>
+<head lang="ru">
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css"  href="../style.css">
+</head>
+<body>
+	<div class="vib">
+	<div id="header">
+		<div class="row-2">
+			<ul>
+				<li><a href="../ruk.pdf" target="_blank">Паспорт ПК</a></li>
+				<li><a href="../Video/index.html">Видеоролик</a></li>
+				<li><a href="../Diagnost/index.html">Диагностика неисправностей</a></li>
+				<li><a href="../Structure/index.html" class="active">Структурная схема организации</a></li>
+				<li class="last"><a href="../Setev/index.html">Сетевая схема</a></li>
+			</ul>
+		</div>
+	</div>
+	</div>
+	<div class="table1">
+<h1>Структурная схема организации</h1>
+<center>
+  <p><img src="scheme.png" align="center" ></p>
+	</center>
+</div>
+<h1>©Толмачев Александр</h1>
+</body>
+</html>
